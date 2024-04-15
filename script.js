4fbb567b3658a12ff2c65b26adf2d1a8ddc094ac
Dnyaneshwar Suryawanshi <144836277+Dnyanesh9881@users.noreply.github.com>
2024-04-15 23:35:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 function firstWord(s) {
   // your code here
+	if(s.trim()===""){
+		return s;
+	}
+	if(!s.trim().includes(" ")){
+		return s;
+	}
+	else{
+		let str=s.trim();
+		let ans="";
+		for (let i = 0; i < str.length; i++) {
+			if(str.charAt(i)!==" "){
+				ans+=str.charAt(i);
+			}else{
+				break;
+			}
+		}
+		return ans;
+	}
 }
 
 // Do not change the code below
